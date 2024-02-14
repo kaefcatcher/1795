62a3e0e0be09f0e4e553c4e2690de9f91063b814
kaefcatcher <shembeld@gmail.com>
2024-02-14 08:45:56 +0300

    hotfix parser

--- parser.sh
+++ parser.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Parse config file
 config_file="config.txt"
 
-# Read config file line by line
 while IFS= read -r line; do
-    # Remove comments
     line=$(echo "$line" | sed 's/#.*//')
 
-    # Extract parameter and its value
     if [[ $line =~ ^([^\s=]+)=([^;]+) ]]; then
         param="${BASH_REMATCH[1]}"
         value="${BASH_REMATCH[2]}"
