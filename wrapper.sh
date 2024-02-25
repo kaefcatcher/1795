317f4bd8087b7f08a569854c3c8fbcc4be758c5c
kaefcatcher <shembeld@gmail.com>
2024-02-25 23:02:23 +0300

    hz nu chet ne rabotaet

--- wrapper.sh
+++ wrapper.sh
@@ -3,7 +3,7 @@
 param=$(./parser.sh "$1")
 
 if [ $? -eq 0 ]; then
-    ./waf --run "scratch/HIGHWAY ($param) --Periodic"
+    ./waf --run "scratch/HIGHWAY ($param) --Periodic tracefile="trace1.xml""
 else
     echo "Error: Parameter '$1' not found in config file."
 fi
